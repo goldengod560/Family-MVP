5496ecdad5a9b4be1c123249639ae7932cfb20f2
goldengod560 <61258733+goldengod560@users.noreply.github.com>
2026-09-07 16:28:14 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'family-nfl-picks-v07';
+const CACHE = 'family-nfl-picks-v07-force-0707';
 const ASSETS = ['./','./index.html','./styles.css','./config.js','./api.mjs','./app.js','./logic.mjs','./report.mjs','./manifest.webmanifest'];
 
 self.addEventListener('install', event => {
